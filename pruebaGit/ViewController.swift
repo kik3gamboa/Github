7fa579080853f1abc950e2319e30847f0c72a0ae
Kike Gamboa <kikeremiau@gmail.com>
2016-02-03 18:22:27 -0600

    a5777e2a0e9de81c15396faa3fdf0c84b8d3044b

--- pruebaGit/ViewController.swift
+++ pruebaGit/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Telecomunicaciones Abiertas de México on 2/3/16.
 //  Copyright © 2016 Telecomunicaciones Abiertas de México. All rights reserved.
-//
+//  a5777e2a0e9de81c15396faa3fdf0c84b8d3044b
 
 import UIKit
 
